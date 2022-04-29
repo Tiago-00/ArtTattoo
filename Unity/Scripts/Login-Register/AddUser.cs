1287f83c71506dfcb9736940801a81f7d7d172f2
Rafaelnuno <64990428+Rafaelnuno@users.noreply.github.com>
2022-04-29 23:40:08 +0100

    client side

---
+++ Unity/Scripts/Login-Register/AddUser.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Net;
+using System.Net.Sockets;
+
+public class AddUser : MonoBehaviour
+{
+    [SerializeField] private string authenticationEndpoint = "https://arttattoo.herokuapp.com/api/users/adduser";
+
+
+    [SerializeField] private TMP_InputField usernameInputField;
+    [SerializeField] private TMP_InputField emailInputField;
+    [SerializeField] private TMP_InputField passwordInputField;
+    [SerializeField] private TextMeshProUGUI alertText;
+    [SerializeField] private Button RegisterButton;
+    public Button goToLoginButton;
+
+    public void OnLoginClick()
+    {
+        alertText.text = "Signing in...";
+        RegisterButton.interactable = false;
+
+        StartCoroutine(TryLogin());
+    }
+
+    void Start()
+    {
+        
+        goToLoginButton.onClick.AddListener(goToLoginTestScene);
+
+    }
+    
+
+    private IEnumerator TryLogin()
+    {
+
+        string nome = usernameInputField.text;
+        string email = emailInputField.text;
+        string password = passwordInputField.text;
+
+        WWWForm form = new WWWForm();
+        form.AddField("use_nome", nome);
+        form.AddField("use_email", email);
+        form.AddField("use_pass", password);
+
+
+
+        UnityWebRequest request = UnityWebRequest.Post(authenticationEndpoint, form);
+        var handler = request.SendWebRequest();
+
+
+        float startTime = 0.0f;
+        while (!handler.isDone)
+        {
+            startTime += Time.deltaTime;
+
+            if (startTime > 10.0f)
+            {
+                break;
+            }
+
+            yield return null;
+        }
+
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            if (request.downloadHandler.text != "Nome ou password incorreta")// Login success
+            {
+                alertText.text = "Welcome ";
+                RegisterButton.interactable = false;
+            }
+            else
+            {
+                alertText.text = "Invalid credentials ";
+                RegisterButton.interactable = true;
+            }
+
+        }
+        else
+        {
+            alertText.text = "Error connecting";
+            RegisterButton.interactable = true;
+        }
+
+        yield return null;
+    }
+
+    void goToLoginTestScene()
+    {
+        SceneManager.LoadScene("Login");
+    }
+}
---
+++ Unity/Scripts/Login-Register/Login.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using TMPro;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Login : MonoBehaviour
+{
+    [SerializeField] private string authenticationEndpoint = "https://arttattoo.herokuapp.com/api/users/login";
+
+
+    [SerializeField] private TMP_InputField usernameInputField;
+    [SerializeField] private TMP_InputField passwordInputField;
+    [SerializeField] private TextMeshProUGUI alertText;
+    public Button loginButton;
+    public Button goToRegisterButton;
+
+    public void OnLoginClick()
+    {
+        alertText.text = "Signing in...";
+        loginButton.interactable = false;
+
+        StartCoroutine(TryLogin());
+    }
+    void Start()
+    {
+        //loginButton.onClick.AddListener(Principal);
+        goToRegisterButton.onClick.AddListener(moveToRegister);
+    }
+
+   
+ private  IEnumerator TryLogin()
+  {
+
+        string nome = usernameInputField.text;
+        string password = passwordInputField.text;
+        Debug.Log(nome);
+        Debug.Log(password);
+
+                WWWForm form = new WWWForm();
+                form.AddField("nome", nome);
+                form.AddField("password", password);
+
+        UnityWebRequest request = UnityWebRequest.Post(authenticationEndpoint, form);
+        yield return request.SendWebRequest();
+
+
+        /*float startTime = 0.0f;
+            while (!handler.isDone)
+                {
+                    startTime += Time.deltaTime;
+
+                    if (startTime > 10.0f)
+                    {
+                        break;
+                    }
+
+                    yield return null;
+                }*/
+
+                Debug.Log(request.downloadHandler.text);
+       
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                if (request.downloadHandler.text != "Nome ou password incorreta")// Login success
+                {
+                    alertText.text = "Welcome ";
+                    loginButton.interactable = false;
+                    //Principal();
+                }
+                else
+                {
+                    alertText.text = "Invalid credentials ";
+                    loginButton.interactable = true;
+                }
+
+            }
+            else
+            {
+                alertText.text = "Error connecting";
+                loginButton.interactable = true;
+            }
+
+            yield return null;
+        
+    }
+
+    void moveToRegister()
+    {
+        SceneManager.LoadScene("AddUser");
+    }
+
+   /* void Principal()
+    {
+        SceneManager.LoadScene("Principal");
+    }*/
+}
+
